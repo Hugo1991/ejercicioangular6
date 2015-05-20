95649f8bd176bca2fe8ea65be6d9cd91ae6b2bd8
cruzmaster <sefi-sefi@hotmail.com>
2015-05-20 21:35:38 +0200

    Plantilla crear cita y perfil de usuario

--- src/main/resources/static/app/routeconfig.js
+++ src/main/resources/static/app/routeconfig.js
@@ -8,15 +8,13 @@ function RouteConfig($routeProvider) {
 	$routeProvider.when('/registro', { templateUrl : "templates/registro.html" });
 	$routeProvider.when('/creacita', { templateUrl : "templates/concertaCita.html" });
 
-	$routeProvider.when('/listamedicos', { templateUrl : "templates/listamedicos.html" });
-	$routeProvider.when('/listacitas', { templateUrl : "templates/listacitas.html" });
-	$routeProvider.when('/listaHospitales', { templateUrl : "templates/listaHospitales.html" });
-	$routeProvider.when('/postlist', { templateUrl : "templates/postlist.html" });
-	$routeProvider.when('/formularioshospitalmedicos', { templateUrl : "templates/formularioshospitalmedicos.html" });
 	
-	$routeProvider.when('/listausuarios', { templateUrl : "templates/listausuarios.html" });
+	$routeProvider.when('/postlist', { templateUrl : "templates/postlist.html" });
+	$routeProvider.when('/perfilusuario', { templateUrl : "templates/perfilusuario.html" });	
+	
 	
 	$routeProvider.when('/usuario/:id', { templateUrl : "templates/usuario.html" });
+	
 	$routeProvider.when('/cita/:id', { templateUrl : "templates/cita.html" });
 	$routeProvider.when('/post/:id', { templateUrl : "templates/post.html" });
 	$routeProvider.when('/medico/:id', { templateUrl : "templates/medico.html" });
